ef6ae184f823bdb43942d2a0a25a45663ea4c5b0
Melya Tremblay <meltremb@student.42quebec.com>
2023-05-31 14:11:58 -0400

    try

--- src/minishell.c
+++ src/minishell.c
@@ -6,15 +6,13 @@
 /*   By: meltremb <meltremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 13:21:07 by meltremb          #+#    #+#             */
-/*   Updated: 2023/05/31 14:04:44 by meltremb         ###   ########.fr       */
+/*   Updated: 2023/05/31 14:11:11 by meltremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-int	main()
+int	main(int argc, char **argv)
 {
-	char	*line_read = NULL;
-
-	line_read = readline ("enter a line");
+	printf("hello world\n");
 }
